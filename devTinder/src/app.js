1928e5a774ac9801fe2512c2beba8fa5fd2d99cf
Saksham Kothari <sakshamkothari8@gmail.com>
2026-04-07 09:46:33 +0530

    updated successfully.

--- devTinder/src/app.js
+++ devTinder/src/app.js
@@ -2,15 +2,30 @@ const express = require("express");
 
 const app = express();
 
+app.use("/user", (req, res) => {
+  res.send("HAHAH");
+});
+
+//.get will match only get calls.
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Saksham", lastName: "Kothari" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data successfully saved to the database.");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("User Deleted Successfully.");
+});
+
 //Handling the req.
 app.use("/test", (req, res) => {
+  //use will match all the http method apo calls to test
   res.send("Test route working");
 });
 
-app.use("/hello", (req, res) => {
-  res.send("Welcome to hello.");
-});
-
+//Starting the server.
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000");
 }); //give port to host.
