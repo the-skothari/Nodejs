0e4a97190eb21bb7cf048a3f862b42e1f45d66da
Saksham Kothari <sakshamkothari8@gmail.com>
2026-04-09 08:09:02 +0530

    updates successfully.

--- devTinder/src/app.js
+++ devTinder/src/app.js
@@ -2,27 +2,31 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user", (req, res) => {
-  res.send("HAHAH");
+// app.use("/admin", (req, res, next) => {
+//   const token = "xyz";
+//   const admin = token === "xyz";
+//   if (!admin) {
+//     res.status(401).send("Unauthorised Access.");
+//   } else {
+//     next();
+//   }
+// });
+
+//for getData.
+
+//for deleteData
+
+app.get("/userData", (req, res) => {
+  //Logic to DB call.
+
+  throw new Error("Error");
+  res.send();
 });
 
-//.get will match only get calls.
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Saksham", lastName: "Kothari" });
-});
-
-app.post("/user", (req, res) => {
-  res.send("Data successfully saved to the database.");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("User Deleted Successfully.");
-});
-
-//Handling the req.
-app.use("/test", (req, res) => {
-  //use will match all the http method apo calls to test
-  res.send("Test route working");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("something went wrong.");
+  }
 });
 
 //Starting the server.
